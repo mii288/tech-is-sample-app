815a4a4892c62c36461c4ff3c7adaf73e93156fe
mii288 <atteeen@gmail.com>
2021-02-21 17:37:27 +0900

    show animals

---
+++ pages/animals.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="animal in animals" :key="animal.name">
+        <a :href="`/${animal.name}`" class="card">
+          <h2>{{animal.name}}<img :src="require(`~/assets/img/${animal.image}`)" height="100"></h2>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      animals: [
+        {name: 'cat', image: 'cat.png'},
+        {name: 'dog', image: 'dog.png'},
+        {name: 'mouse', image: 'mouse.png'},
+        {name: 'elephant', image: 'elephant.png'}
+      ]
+    }
+  }
+}
+</script>
+
